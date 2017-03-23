c40dd8d9558aaa28460adacc4950533fa8bcc4a1
amrutharamesh <docdagny@gmail.com>
2017-03-23 19:14:39 -0400

    2nd commit

--- js/main.js
+++ js/main.js
@@ -1,82 +1,80 @@
 //Required modules
-var casper = require('casper').create({
-	verbose : true
-});
-var utils = require('utils');
 var fs = require('fs');
+var casper = require('casper').create({
+    verbose : true
+});
+
+//Variables
+var currentLink = 0;
 
 
-//Variables decalaration
-results = [];
-
-/*
-* Functions to read from SSO sites
-*/
-//Read websites from CSV
+/* --------------------------------------------- Helper functions start --------------------------------------------------- */
+//Function to read links from CSV file
 function readWebsitesFromCSV(){
-	websites = [];
+    websites = [];
 
-	//Script starts
-	stream = fs.open('../data/summa.csv','r');
-	line = stream.readLine().split(',')[1];
-	websites.push("https://www."+line);
+    //Script starts
+    stream = fs.open('../data/summa.csv','r');
+    line = stream.readLine().split(',')[1];
+    websites.push("https://www."+line);
 
-	while(line){
-		line = stream.readLine().split(',')[1];
-		websites.push("https://www."+line);
-	}
-	stream.flush();
-	stream.close();
-	return websites;
+    while(line){
+        line = stream.readLine().split(',')[1];
+        websites.push("https://www."+line);
+    }
+    stream.flush();
+    stream.close();
+    return websites;
 }
 
-/*
-* Functions to process webpage for SSO
-*/
-function search(){
-	//Variables declaration
-	candidateSSO = {"loginSSO" : [], "signupSSO" : []};
-	var links = document.querySelectorAll('a');
-    return Array.prototype.map.call(links, function(e) {
-        return e.getAttribute('href');
+// Get the links, and add them to the links array
+function addLinks(link) {
+    this.then(function() {
+        var found = this.evaluate(searchLinks);
+        this.echo(found.length + " links found on " + link);
     });
 }
 
+// Just opens the page and prints the title
+function start(link) {
+    this.start(link, function() {
+        this.echo('Page title: ' + this.getTitle());
+    });
+}
 
+// As long as it has a next link, and is under the maximum limit, will keep running
+function check() {
+    if (websites[currentLink]) {
+        this.echo('--- Link ' + currentLink + ' ---');
+        start.call(this, websites[currentLink]);
+        addLinks.call(this, websites[currentLink]);
+        currentLink++;
+        this.run(check);
+    } else {
+        this.echo(websites);
+        this.echo("All done.");
+        this.exit();
+    }
+}
+/* ---------------------------------------- Helper functions end ------------------------------------------------------------  */
 
-/*
-*	Function calls and program start here
-*/
+/* ---------------------------------------------------- Search functions start ----------------------------------------------- */ 
+function search() {
+    var filter, map;
+    filter = Array.prototype.filter;
+    map = Array.prototype.map;
+    return map.call(filter.call(document.querySelectorAll("a"), function(a) {
+        return (/^http:\/\/.*/i).test(a.getAttribute("href"));
+    }), function(a) {
+        return a.getAttribute("href");
+    });
+}
 
-//Start casper instance
+/* ---------------------------------------------------- Search functions end ----------------------------------------------- */
 
-
-//Call function to read from websites
-websites = readWebsitesFromCSV();
-
-casper.start().each(websites, function(self, site){
-	this.echo(site);
-	self.thenOpen(site, function(){
-		this.echo(self.getTitle());
-		title = this.evaluate(search());
-	});
-	self.then(function(){
-		this.echo(title);
-	});
+/* ------------------------------------Function calls and program start here ------------------------------------------------  */
+casper.start().then(function() {
+    this.echo("Starting");
 });
-// casper.then(function(){
-// 	while(websites.length > 0){
-// 		current = websites.pop();
-// 		this.echo(current);
-// 		candidate = search("https://www."+current);
-// 		if(candidate != null || candidate != {} || !(candidate['loginSSO'].length == 0 && candidate['signupSSO'].length ==0)){
-// 			results.push(candidate);
-// 		}
-// 	}
-// });
-casper.run(function(){
-	this.echo("hi");
-	this.exit();
-});
 
-
+casper.run(check);
