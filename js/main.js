a08cb5f06de156216bc59cb02865763deeef09e9
amrutharamesh <docdagny@gmail.com>
2017-03-30 17:35:31 -0500

    fixesss

--- js/main.js
+++ js/main.js
@@ -3,21 +3,22 @@ var fs = require('fs');
 var casper = require('casper').create({
     verbose : true,
     logLevel : 'info',
-    stepTimeout : 30000,
+    stepTimeout : 3000,
     pageSettings : {
         loadPlugins : false,
         userAgent : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
         ignoreSslErrors: true
     },
-    onStepTimeout : function(step, timeout){
+    onStepTimeout : function(timeout, step){
         this.echo("Step nummm" + step);
-        this.echo(this.page.url)
+        this.echo(this.page.url);
         this.echo(total);
         if(step == 1){
             total += timeout;
             loading = this.page.loadingProgress;
-            if(total >= 90000 && loading < 90){
-                this.page.close();
+            if(total >= 60000 && loading < 90){
+                this.clear();
+                this.page.stop();
                 this.echo("timed out");
             }
         }
