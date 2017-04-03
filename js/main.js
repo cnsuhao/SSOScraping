4321616ba8062891525d1f53e261fb5c0c43c4f0
amrutharamesh <docdagny@gmail.com>
2017-04-02 21:45:31 -0500

    fixes

--- js/main.js
+++ js/main.js
@@ -7,8 +7,11 @@ var casper = require('casper').create({
     pageSettings : {
         loadImages: false,
         loadPlugins: false,
-        userAgent: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
+        userAgent: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+        XSSAuditingEnabled : true
     },
+    exitOnError : false,
+    silentErrors : true,
     onStepTimeout : function(timeout, step){
         if(step == 1){
             total += timeout;
