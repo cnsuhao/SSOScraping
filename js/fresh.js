6006dd55cc07e61e7205a7bc57531a97ac8d90f9
amrutharamesh <docdagny@gmail.com>
2017-04-05 18:21:38 -0500

    slimer try

---
+++ js/fresh.js
@@ -0,0 +1,51 @@
+var linknum = 0;
+var sites = [];
+
+var fs = require('fs');
+sites = readWebsitesFromCSV();
+
+var casper = require('casper').create({
+	verbose : true,
+	logLevel : 'debug'
+});
+
+casper.start().then(function(){
+    this.echo("heeee"+sites)
+	this.echo(require.paths)
+});
+
+
+casper.run(check)
+
+
+function start(link){
+    this.start(link, function(){
+        this.echo("Page title" + this.getTitle());
+    });
+}
+function check(){
+    if(sites[linknum]){
+        var current = sites[linknum];
+        start.call(this, current)
+        linknum++;
+        this.run(check);
+    }else{
+        this.echo("All done")
+        this.exit();
+    }
+}
+
+
+
+ 
+// //Function to read links from CSV file
+function readWebsitesFromCSV(){
+    //Script starts
+    var stream = fs.open('../data/summa.csv','r');
+    var reading = stream.read().split(/\r?\n/);
+    var result = reading.map(function(val, index, arr){
+        return val = "https://www." + val.split(',')[1];
+    });
+    stream.close();
+    return result;
+}
