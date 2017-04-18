7773744cf55ed18af0fd675bf48852a474a5739e
amrutharamesh <docdagny@gmail.com>
2017-04-18 15:31:10 -0500

    20k runs

--- js/index.js
+++ js/index.js
@@ -230,7 +230,7 @@ function run(array){
 				                    	if(each.site != 'box' && each.site != 'vk' && each.site != '500px'){
 				                    		if(inputstr.match(k2) != null || inputstr.match(k3) != null  || inputstr.match(k4) != null || 
 					                            inputstr.match(k5) != null || inputstr.match(k6) != null){
-					                            return each.site+', edgecase';
+					                            return each.site+', no-url';
 					                        }
 				                    	} 
 				                    }
