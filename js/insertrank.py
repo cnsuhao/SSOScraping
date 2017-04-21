9481a9d83327d5882be1f3e2523d993c96aa5e05
amrutharamesh <docdagny@gmail.com>
2017-04-21 16:08:07 -0500

    20k runs

--- js/insertrank.py
+++ js/insertrank.py
@@ -2,6 +2,7 @@ import sys
 import json
 import csv
 import collections
+import ast
 
 errors = []
 runs = []
@@ -12,15 +13,6 @@ filename = sys.argv[1]
 fname = str(filename)+'/'+str(filename)+'.csv_log.txt'
 csvname = str(filename)+'.csv'
 
-def convert(data):
-    if isinstance(data, basestring):
-        return data.encode('utf-8').strip()
-    elif isinstance(data, collections.Mapping):
-        return dict(map(convert, data.iteritems()))
-    elif isinstance(data, collections.Iterable):
-        return type(data)(map(convert, data))
-    else:
-        return data
 
 with open('../data/csv/5k groups/'+str(csvname)) as csvFile:
     reader = csv.reader(csvFile, delimiter=",")
@@ -29,11 +21,8 @@ with open('../data/csv/5k groups/'+str(csvname)) as csvFile:
     print(len(websites))
     csvFile.close()
 
-with open('../data/Runs - success/'+fname) as tfile:
-    fileStr = tfile.readlines()
-    for i in fileStr:
-        jsn = convert(json.loads(i))
-        print jsn.keys()
+
+        
  
 
 
