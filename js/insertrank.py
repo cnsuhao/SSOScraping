c8a045659b8f498a35217aa6c68589d15f200440
amrutharamesh <docdagny@gmail.com>
2017-04-21 19:21:36 -0500

    20k runs

--- js/insertrank.py
+++ js/insertrank.py
@@ -24,9 +24,13 @@ with open('../data/csv/5k groups/'+str(csvname)) as csvFile:
 
 with open('../data/Runs - success/'+fname) as tfile:
     for line in tfile:
-        split = ast.literal_eval(line)
-        print type(split)
- 
-
+        line = line.replace("\"", "'")
+        jsn = json.loads((json.dumps(line)))
+        nwd = ast.literal_eval(jsn)
+        keys = []
+        for i in nwd:
+            keys.append(i)
+            print nwd[i]
+        print keys
 
 
