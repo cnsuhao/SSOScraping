de14f1c51787a045e613f4145fb6eb1585e9393b
amrutharamesh <docdagny@gmail.com>
2017-05-12 22:32:27 -0400

    changes

---
+++ js/runMain.py
@@ -0,0 +1,15 @@
+import csv
+import os
+import json
+import subprocess
+import sys
+
+
+
+os.environ["DISPLAY"]=":3"
+
+
+filename = sys.argv[1]
+logName = sys.argv[2]
+cmd = "node main.js -w 2'"+json.dumps(filename)+" | tee -a '"+json.dumps(logName)+"'"
+subprocess.call(cmd, shell=True)
