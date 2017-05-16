0b3c3f5174f1349406f3c99bd7cf814b1c6da044
amrutharamesh <docdagny@gmail.com>
2017-05-16 16:00:02 -0400

    New csvs

--- js/all.sh
+++ js/all.sh
@@ -18,3 +18,24 @@ python ./new.py 360k-370k.csv 360-370k_log.txt | tee -a 360k-370k_new.csv
 python ./new.py 370k-380k.csv 370-380k_log.txt | tee -a 370k-380k_new.csv
 python ./new.py 380k-390k.csv 380-390k_log.txt | tee -a 380k-390k_new.csv
 python ./new.py 390k-400k.csv 390-400k_log.txt | tee -a 390k-400k_new.csv
+
+python ./split10k.py 600k-610k.csv 600000 610000
+python ./split10k.py 610k-620k.csv 610000 620000
+python ./split10k.py 620k-630k.csv 620000 630000
+python ./split10k.py 630k-640k.csv 630000 640000
+python ./split10k.py 640k-650k.csv 640000 650000
+python ./split10k.py 650k-660k.csv 650000 660000
+python ./split10k.py 660k-670k.csv 660000 670000
+python ./split10k.py 670k-680k.csv 670000 680000
+python ./split10k.py 680k-690k.csv 680000 690000
+python ./split10k.py 690k-700k.csv 690000 700000
+python ./split10k.py 700k-710k.csv 700000 710000
+python ./split10k.py 710k-720k.csv 710000 720000
+python ./split10k.py 720k-730k.csv 720000 730000
+python ./split10k.py 730k-740k.csv 730000 740000
+python ./split10k.py 740k-750k.csv 740000 750000
+python ./split10k.py 750k-760k.csv 750000 760000
+python ./split10k.py 760k-770k.csv 760000 770000
+python ./split10k.py 770k-780k.csv 770000 780000
+python ./split10k.py 780k-790k.csv 780000 790000
+python ./split10k.py 790k-800k.csv 790000 800000
