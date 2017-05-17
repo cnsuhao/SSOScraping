1dd559b59c948d3c6efa0a3c8eae8795c3341cda
amrutharamesh <docdagny@gmail.com>
2017-05-17 17:41:19 -0400

    data

--- js/split10k.py
+++ js/split10k.py
@@ -3,21 +3,31 @@ import os
 import sys
 
 websites = []
-filename = sys.argv[1]
-start = sys.argv[2]
-end = sys.argv[3]
+# filename = sys.argv[1]
+# start = sys.argv[2]
+# end = sys.argv[3]
 
 # Read websites data from CSV file
-with open('../data/csv/top-1m.csv') as csvFile:
+with open('error4.csv') as csvFile:
     reader = csv.reader(csvFile, delimiter=",")
     for data in reader:
         websites.append(data)
     csvFile.close()
 
 print len(websites)
-with open(str(filename), "wb") as file:
-	writer = csv.writer(file,  delimiter=',')
-	for data in websites:
-		if(int(data[0]) > int(start) and int(data[0]) <= int(end)):
+
+# Split into chunks of array
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in xrange(0, len(l), n):
+        yield l[i:i + n]
+
+
+listchunks = list(chunks(websites, 400));
+for idx,chunk in enumerate(listchunks):
+	fname = "400-"+str(idx+1)+"k.csv"
+	with open(fname, "wb") as file:
+		writer = csv.writer(file,  delimiter=',')
+		for data in chunk:
 			writer.writerow(data)
-	file.close()
+		file.close()
